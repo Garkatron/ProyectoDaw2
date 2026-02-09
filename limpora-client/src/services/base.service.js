5c8daac5ac1ba5082e712cec6f3ab72d5887ad75
Garkatron <masitacarg@gmail.com>
2026-02-09 12:37:31 +0000

    fix stuff?

--- limpora-client/src/services/base.service.js
+++ limpora-client/src/services/base.service.js
@@ -3,10 +3,10 @@ import axios from "axios";
 
 const API_URL =
   process.env.NODE_ENV === "production"
-    ? "http://limpora-server:3000/api/v1" 
+    ? "http://localhost:3001/api/v1" 
     : "/api/v1"; 
 
 export const API = axios.create({
-  baseURL: "http://limpora-server:3000/api/v1",
+  baseURL: API_URL,
   withCredentials: true
 });
