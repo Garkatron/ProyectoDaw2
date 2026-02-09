854000ad52a9f81a5e50fef8b02e0ea7e7d6a0ca
Garkatron <masitacarg@gmail.com>
2026-02-09 14:13:08 +0000

    fix stuff?

--- limpora-client/vite.config.js
+++ limpora-client/vite.config.js
@@ -35,29 +35,13 @@ export default defineConfig({
 
   },
   preview: {
-    allowedHosts: true,
     host: true,
     port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://limpora-server:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
-        secure: false,
-        ws: true,
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
-      },
-    },
+    allowedHosts: [
+      'www.limpora.xyz',
+      'limpora.xyz',
+      'matias.informaticamajada.es',
+    ],
   },
   hmr: {
     host: 'localhost',
