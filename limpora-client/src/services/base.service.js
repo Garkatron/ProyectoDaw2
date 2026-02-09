e48f30e801f58bc96c95c6720545d598e6865783
Garkatron <masitacarg@gmail.com>
2026-02-09 12:11:02 +0000

    fix client proxy port

--- limpora-client/src/services/base.service.js
+++ limpora-client/src/services/base.service.js
@@ -1,7 +1,12 @@
 
 import axios from "axios";
 
+const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "http://limpora-server:3000/api/v1" 
+    : "/api/v1"; 
+
 export const API = axios.create({
-  baseURL: "/api/v1",
+  baseURL: API_URL,
   withCredentials: true
 });
