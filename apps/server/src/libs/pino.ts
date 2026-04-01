de8ab6653aff17bcda364ac3609b12d3c86736f1
Garkatron <masitacarg@gmail.com>
2026-04-01 22:23:17 +0100

    Trying to fix logging

--- apps/server/src/libs/pino.ts
+++ apps/server/src/libs/pino.ts
@@ -1,16 +1,16 @@
 import pino from 'pino'
-import PinoLoki from 'pino-loki';
-
+import PinoLoki from 'pino-loki'
 
 const stream = PinoLoki({
   host: 'http://loki:3100',
   labels: { app: 'limpora-server', env: 'production' },
   propsToLabels: ['level'],
   silenceErrors: false,
-  batching: {
-    interval: 5,
-    maxBufferSize: 1000,
-  },
+  batching: false,
 })
 
+stream.on('error', (err: any) => console.error('PINO-LOKI ERROR:', err))
+
 export const logger = pino({ level: 'info' }, stream)
+
+logger.info('pino-loki startup test')
