cd3e32e52a7e3a495627b93402d5762c7c9ad82b
Garkatron <masitacarg@gmail.com>
2026-04-01 21:55:32 +0100

    trying to fix pino loki ;_;

--- apps/server/src/libs/pino.ts
+++ apps/server/src/libs/pino.ts
@@ -1,14 +1,14 @@
 import pino from "pino";
 
 export const logger = pino({
-    level: "info",
-    transport: {
-        target: "pino-loki",
-        options: {
-            batching: true,
-            interval: 5, 
-            labels: { app: "limpora-server" },
-            host: "http://127.0.0.1:3100", 
-        },
+  level: "info",
+  transport: {
+    target: "pino-loki",
+    options: {
+      batching: true,
+      interval: 5, 
+      labels: { app: "limpora-server" },
+      host: "http://loki:3100", 
     },
+  },
 });
