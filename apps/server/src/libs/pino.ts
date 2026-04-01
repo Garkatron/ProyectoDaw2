85cb6d0bd1ca36c65b930b1bd10a1f2e00a7de43
Garkatron <masitacarg@gmail.com>
2026-04-01 22:20:29 +0100

    Trying to fix logging

--- apps/server/src/libs/pino.ts
+++ apps/server/src/libs/pino.ts
@@ -1,16 +1,16 @@
 import pino from 'pino'
-import type { LokiOptions } from 'pino-loki'
+import PinoLoki from 'pino-loki';
 
-const transport = pino.transport<LokiOptions>({
-  target: 'pino-loki',
-  options: {
-    host: 'http://loki:3100',
-    labels: { app: 'limpora-server', env: 'production' },
-    propsToLabels: ['level'],
-    batching: {
-      interval: 5,
-      maxBufferSize: 1000,
-    },
+
+const stream = PinoLoki({
+  host: 'http://loki:3100',
+  labels: { app: 'limpora-server', env: 'production' },
+  propsToLabels: ['level'],
+  silenceErrors: false,
+  batching: {
+    interval: 5,
+    maxBufferSize: 1000,
   },
 })
-export const logger = pino(transport)
+
+export const logger = pino({ level: 'info' }, stream)
