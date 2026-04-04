a661fa9f0dcea1e7267bd7f7a0e2aeebaec6a1d3
Garkatron <masitacarg@gmail.com>
2026-04-04 20:59:14 +0100

    Trying to fix client

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.js";
 import { BrowserRouter } from "react-router-dom";
-import { MantineProvider, ColorSchemeScript, createTheme } from "@mantine/core";
 import { mantineTheme } from "./themes/theme.js";
-import "@mantine/core/styles.css";
-import '@mantine/dates/styles.css';
+
 import "./i18n";
+import { MantineProvider } from '@mantine/core';
 
 const root = document.getElementById("root");
 if (!root) throw new Error("Root element not found");
