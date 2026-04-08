eb84b0eb440a0f94711e8babb0ad37a7860552ac
Garkatron <masitacarg@gmail.com>
2026-04-08 20:25:47 +0100

    More security stuff

--- apps/server/src/libs/redis.ts
+++ apps/server/src/libs/redis.ts
@@ -2,7 +2,11 @@ import { createClient } from "redis";
 
 const redis = createClient({
     url: "redis://redis:6379",
+    password: process.env.REDIS_PASSWORD,
+    
 });
 
 await redis.connect();
 export default redis;
+
+
