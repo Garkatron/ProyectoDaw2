bad87a2425c72cf98ec2b6c0a358cfc12cdb7d11
Garkatron <masitacarg@gmail.com>
2026-04-08 22:12:49 +0100

    trying to fix settings

--- apps/client/src/pages/main/Settings.tsx
+++ apps/client/src/pages/main/Settings.tsx
@@ -94,7 +94,7 @@ export default function Settings() {
                     ]}
                     radius="xl"
                     size="xs"
-                    mr="xl"
+                    style={ { minWidth: "380px" } }
                   />
                 }
               />
